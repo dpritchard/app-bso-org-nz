985d2bc8bc09c98815a60799c951c40183b3717a
Daniel Pritchard <daniel@pritchard.co>
2018-09-23 18:22:10 +1200

    Update sidebar

--- resources/views/partials/main_nav.blade.php
+++ resources/views/partials/main_nav.blade.php
@@ -1,12 +1,12 @@
-<a class="nav-link" href="/">BSO Home</a>
+<a class="nav-link" href="/">Home</a>
 <a class="nav-link" href="/diary">Diary</a>
 <a class="nav-link" href="/past-events">Past Events</a>
 <a class="nav-link" href="/trip-guidelines">Trip Guidelines</a>
-<a class="nav-link" href="/bso-calendar">2018 Calendar sold out</a>
-<a class="nav-link" href="/photo-competition">2018 Photo Competition</a>
-<a class="nav-link" href="/eagle-drawing-competiton">Audrey Eagle Drawing Competiton</a>
+<a class="nav-link" href="/bso-calendar">Calendar</a>
+<a class="nav-link" href="/photo-competition">Photo Competition</a>
+<a class="nav-link" href="/eagle-drawing-competiton">Drawing Competiton</a>
 <a class="nav-link" href="/lichen-guide">Lichen Guide</a>
-<a class="nav-link" href="/pbfund">The Peter Bannister Student Field Grant</a>
+{{-- <a class="nav-link" href="/pbfund">The Peter Bannister Student Field Grant</a> --}}
 <a class="nav-link" href="/newsletter"><i>Newsletter</i></a>
 <a class="nav-link" href="/eagle-supplement">Eagle Supplements</a>
 <a class="nav-link" href="/about-joining-the-bso">Join the BSO</a>
@@ -14,6 +14,10 @@
 <a class="nav-link" href="/links">Links</a>
 <a class="nav-link" href="/downloads">Downloads</a>
 <a class="nav-link" href="/disclamer">Disclaimer</a>
+
+<a class="nav-link" href="https://www.facebook.com/pages/Botanical-Society-of-Otago/513831728707617">
+<img alt="FindUsOnFacebook" class="facebook-logo" src="{{ secure_asset('img/FindUs-FB-RGB-BRC-Site-500.png') }}">
+</a>
 
 @auth
 <a class="nav-link" href="{{ route('logout') }}"
