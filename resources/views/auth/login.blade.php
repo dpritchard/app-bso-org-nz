88e5453acd52a981987fc226a1a8be70f1d99a2b
Daniel Pritchard <daniel@pritchard.co>
2023-12-07 10:58:16 +1300

    Minor changes to Admin styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
+                        <div class="row mb-2">
                             <label for="email" class="col-md-4 col-form-label text-md-end form-label">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
@@ -25,9 +25,8 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="row mb-2">
                             <label for="password" class="col-md-4 col-form-label text-md-end form-label">{{ __('Password') }}</label>
-
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
@@ -39,7 +38,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="row mb-2">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -51,7 +50,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
