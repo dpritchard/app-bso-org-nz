366b1ac6e6f37f9a99abfaf6457eb7f8768eb37d
Daniel Pritchard <daniel@pritchard.co>
2025-06-09 19:33:13 +1200

    Photo comp 20205 winners

--- resources/views/photo_competition/2025.blade.php
+++ resources/views/photo_competition/2025.blade.php
@@ -4,7 +4,7 @@
 <div class="mb-5">
 	<h1>2025 Photo Competition</h1>
 	<!-- Notes for 2026... Need to streamline this: Short Title. Author. Caption (optional). Set data in php and pass to JS. Supress author until voting finished.-->
-
+<!--
 	<p>Welcome to the 2025 BSO Photo Competition. This year we have received 48 photos, from 11 photographers, across the 3 categories.</p>
 
 	<p>Our judges will decide the overall category winners and their decision will be announced on May 14 2025 at the AGM. However, we also have a <em>People’s Choice Award</em> which is up to you!</p>
@@ -12,46 +12,40 @@
 	<h3>Voting Information</h3>
 	<p>BSO members may vote for as many as three of their favourite photos (votes are not limited to one per category). You may browse the photos by clicking on the category buttons below. You can optionally assemble a shortlist of favourites, from which you can select up to three photos to vote for.</p>
 	<p>Note that this webpage does not record your votes directly - it simply helps you browse and select your favourites. Information on how to cast your vote via email will be displayed once you have selected at least one photo to vote for.</p>
+ -->
 
-<!-- 
 	<p>Welcome to the 2026 BSO Photo Competition. This year, we received 48 photos, from 11 photographers, across the 3 categories.</p>
 
 	<p>Our judges announced the overall category winners on May 14 2025 at the AGM. At the same time we announced the <em>People’s Choice Award</em> chosen by BSO members.</p>
 
 	<h3>Category Winner: Plant Portrait</h3>
+	<p>Also joint winner for the <em>People’s Choice Award</em></p>
 	<figure class="figure">
-		<img src="" class="figure-img img-fluid rounded" alt="">
-		<figcaption class="figure-caption"></figcaption>
+		<img src="https://bso.org.nz/f/d849dx/1_4_dune_gentian_rakiura.jpg" class="figure-img img-fluid rounded" alt="">
+		<figcaption class="figure-caption">Dune gentian, Rakiura by John Barkla.</figcaption>
 	</figure>
 
 	<h3>Category Winner: Plants in the Landscape</h3>
 	<figure class="figure">
-		<img src="" class="figure-img img-fluid rounded" alt="">
-		<figcaption class="figure-caption"></figcaption>
+		<img src="https://bso.org.nz/f/8lndwx/2_14_celmisa_in_st._marys.jpg" class="figure-img img-fluid rounded" alt="">
+		<figcaption class="figure-caption"><i>Celmisia</i> in St. Marys by Rachael Baxter.</figcaption>
 	</figure>
 
 
 	<h3>Category Winner: Life Under the Lens</h3>
+	<p>Also joint winner for the <em>People’s Choice Award</em></p>
 	<figure class="figure">
-		<img src="" class="figure-img img-fluid rounded" alt="">
-		<figcaption class="figure-caption"></figcaption>
-	</figure>
-
-	<h3><em>People’s Choice Award</em></h3>
-	<figure class="figure">
-		<img src="" class="figure-img img-fluid rounded" alt="">
-		<figcaption class="figure-caption"></figcaption>
+		<img src="https://bso.org.nz/f/xj6k6q/3_8_fruiting_phyllosphere.jpg" class="figure-img img-fluid rounded" alt="">
+		<figcaption class="figure-caption"><i>Hymenophyllum</i> species supporting life by Jo Sinclair.</figcaption>
 	</figure>
 
 	<h3>Viewing Information</h3>
-	<p>We set this page up for members choice voting but now you may now use it to browse the photos by clicking on the category buttons below. You can optionally assemble a shortlist of favourites.</p> 
--->
-
+	<p>We set this page up for members choice voting but now you may now use it to browse the photos by clicking on the category buttons below. You can optionally assemble a shortlist of favourites.</p>
 
 </div>
 <div class="d-flex align-items-center">
 	<v-photo-vote
-	v-bind:allow-votes="true"
+	v-bind:allow-votes="false"
     v-bind:categories="[
     	{code: 1, name: 'Plant Portrait', description: ''},
     	{code: 2, name: 'Plants in the Landscape', description: ''},
@@ -94,7 +88,7 @@
 {f: 'https://bso.org.nz/f/4866k8/2_11_sticking_out.jpg', c: 'Alpine plants on Mount Lyndon, Canterbury'},
 {f: 'https://bso.org.nz/f/jx7mbx/2_12_rain_forest.jpg', c: 'A diverse assemblage of plants thrives in a damp corner of a South Otago forest.'},
 {f: 'https://bso.org.nz/f/841jd8/2_13_hope.jpg', c: 'Natural regen of Podocarpus laetus seedling in front of ancient tōtara stump in Waikerikeri Conservation area '},
-{f: 'https://bso.org.nz/f/8lndwx/2_14_celmisa_in_st._marys.jpg', c: 'Celmisia in St. Marys '},
+{f: 'https://bso.org.nz/f/8lndwx/2_14_celmisa_in_st._marys.jpg', c: 'Celmisia in St. Marys'},
 {f: 'https://bso.org.nz/f/gqobw8/2_15_a_rock_s_view.jpg', c: 'Hump ridge view'},
 {f: 'https://bso.org.nz/f/lq2v38/2_16_a_frame_for_a_tarn.jpg', c: 'Tarn on hump ridge track'},
 {f: 'https://bso.org.nz/f/n8wv1x/3_1_olearia_laxiflora.jpg', c: 'Flower detail'},
